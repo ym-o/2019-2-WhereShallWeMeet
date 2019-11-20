2177a222f6c8459708b5a07604be94fa3db48da7
ans4572 <ans4572@naver.com>
2019-11-20 18:27:05 +0900

    Add files via upload (#2)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express'); // 웹 서버 사용
-var fs = require('fs'); // 파일 로드 사용
+var fs = require('fs') // 파일 로드 사용
 
 var app = express();
 
@@ -11,8 +11,9 @@ app.listen(port, function(){
     console.log('Server Start, Port : ' + port);
 });
 
+// main.html - 메인 페이지
 app.get('/', function(req, res){
-    fs.readFile('index1.html', function(error, data){
+    fs.readFile('main.html', function(error, data){
         if(error){
             console.log(error);
         }else{
@@ -22,8 +23,45 @@ app.get('/', function(req, res){
     });
 });
 
+// info.html - 참석자 정보 입력 페이지
 app.get('/info', function(req, res){
-    fs.readFile('index2.html', function(error, data){
+    fs.readFile('info.html', function(error, data){
+        if(error){
+            console.log(error);
+        }else{
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        }
+    });
+});
+
+// midmap.html - 중간 지점 표시 페이지
+app.get('/mid', function(req, res){
+    fs.readFile('midmap.html', function(error, data){
+        if(error){
+            console.log(error);
+        }else{
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        }
+    });
+});
+
+// places.html - 장소 및 활동 추천 페이지
+app.get('/place', function(req, res){
+    fs.readFile('places.html', function(error, data){
+        if(error){
+            console.log(error);
+        }else{
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        }
+    });
+});
+
+// history.html - history 페이지
+app.get('/history', function(req, res){
+    fs.readFile('history.html', function(error, data){
         if(error){
             console.log(error);
         }else{
