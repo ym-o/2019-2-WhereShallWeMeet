b8606cbae30aab2e4934f354560ee4a8dd3efad7
ans4572 <ans4572@naver.com>
2019-12-02 12:37:24 +0900

    DB연동 및 info에서 모임 정보 DB로 저장

--- app.js
+++ app.js
@@ -1,54 +1,11 @@
+const http = require('http');
 
-/**
- * Module dependencies.
- */
+const hostname = '127.0.0.1';
+const port = 1337;
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-//var test = require('./routes/test');
-var http = require('http');
-var path = require('path');
-
-var app = express();
-
-// all environments
-app.set('port', process.env.PORT || 8080);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-app.get('/test', require('./routes/test').get);
-app.post('/test', require('./routes/test').post);
-
-app.get('/info', require('./routes/info').get);
-app.post('/info', require('./routes/info').post);
-
-app.get('/midmap', require('./routes/midmap').get);
-app.get('/places', require('./routes/places').get);
-
-
-
-app.use(function(req,res){
-    res.render('404.ejs');
-});
-
-
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
--- routes/user.js
+++
@@ -1,9 +0,0 @@
-
-/*
- * GET users listing.
- */
-
-exports.list = function(req, res){
-  //console.log(req.param('id'));
-  res.send("respond with a resource");
-};
