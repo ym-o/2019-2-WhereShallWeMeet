4a40f29c418ad0e261a0f76cffe6476420cbb45f
ym-o <55729131+ym-o@users.noreply.github.com>
2019-12-05 01:07:42 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,128 +1,91 @@
 var express = require('express'); // 웹 서버 사용
 var fs = require('fs'); // 파일 로드 사용
-var bodyParser = require('body-parser');
+
 var app = express();
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
 app.use(express.static('public'));
 
-var port = 8080;
+var port = 8080;  // port number
 
-//DATABASE SETTING
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-  host: "whereshallwemeet-db.cxotycrbhaji.ap-northeast-2.rds.amazonaws.com",
-  port: 3306,
-  user: "jisoo449",
-  password: "rea!choc5",
-  database: "users"
+app.listen(port, function(){
+    console.log('Server Start, Port : ' + port);
 });
+/*
+class Category {
+  Generator({category}){
+    this.category = category;
+  }
+  selecCategory(category){
+    return this.category;
+  }
+  bringPlacelist(places){
 
-//connection.connect(function(err) {
-//  if (err) {
-//    throw err;
-//  } else {
-//    connection.query("SELECT * FROM user1", function(err, rows, fields) {
-//      console.log(rows);
-//    });
-//  }
-//});
+  }
+}
 
-app.listen(port, function() {
-  console.log('Server Start, Port : ' + port);
-});
+class Places{
 
+}
+*/
+/*
+*************************************************************
+버튼 사용하실 때
+<a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="/info">START</a>
+이런 식이면 href 안에 내용을 아래 중 알맞은 걸로 작성하시면 됩니다.
+*/
 // main.html - 메인 페이지
-app.get('/', function(req, res) {
-  fs.readFile('main.ejs', function(error, data) {
-    if (error) {
-      console.log(error);
-    } else {
-      res.writeHead(200, {
-        'Content-Type': 'text/html'
-      });
-      res.end(data);
-    }
-  });
-  //클라이언트 ip 받아오기
-  //var ip = (req.headers['x-forwarded-for'] ||
-  //  req.connection.remoteAddress ||
-  //  req.socket.remoteAddress ||
-  //  req.connection.socket.remoteAddress).split(",")[0];
+app.get('/', function(req, res){
+    fs.readFile('main.ejs', function(error, data){
+        if(error){
+            console.log(error);
+        }else{
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        }
+    });
 });
 
 // info.html - 참석자 정보 입력 페이지
-app.get('/info', function(req, res) {
-  fs.readFile('info.ejs', function(error, data) {
-    if (error) {
-      console.log(error);
-    } else {
-      res.writeHead(200, {
-        'Content-Type': 'text/html'
-      });
-      res.end(data);
-    }
-  });
-});
-
-// info에서 모임 정보를 데이터베이스로 넘기기
-app.post('/info', function(req, res) {
-  var body = req.body;
-  var names = body.name[0];
-  for (var i = 1; i < 8; ++i) {
-    if (!body.name[i]){
-      names += " *";
-      break;
-    }
-    names += " | " + body.name[i];
-  }
-  console.log(names);
-  connection.query('insert into test(meeting,day,attendees) values(?,?,?)', [body.meet, body.date, names],
-    function(error, result) {
-      if (error) {
-        console.log('insert error : ', error);
-      } else {
-        res.redirect('/mid');
-      }
+app.get('/info', function(req, res){
+    fs.readFile('info.ejs', function(error, data){
+        if(error){
+            console.log(error);
+        }else{
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        }
     });
 });
 
 // midmap.html - 중간 지점 표시 페이지
-app.get('/mid', function(req, res) {
-  fs.readFile('midmap.ejs', function(error, data) {
-    if (error) {
-      console.log(error);
-    } else {
-      res.writeHead(200, {
-        'Content-Type': 'text/html'
-      });
-      res.end(data);
-    }
-  });
+app.get('/mid', function(req, res){
+    fs.readFile('midmap.ejs', function(error, data){
+        if(error){
+            console.log(error);
+        }else{
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        }
+    });
 });
 
 // places.html - 장소 및 활동 추천 페이지
-app.get('/place', function(req, res) {
-  fs.readFile('places.ejs', function(error, data) {
-    if (error) {
-      console.log(error);
-    } else {
-      res.writeHead(200, {
-        'Content-Type': 'text/html'
-      });
-      res.end(data);
-    }
-  });
+app.get('/place', function(req, res){
+    fs.readFile('places.ejs', function(error, data){
+        if(error){
+            console.log(error);
+        }else{
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        }
+    });
 });
 
 app.post('/place', function(req, res) {
   var body = req.body;
   for (var i = 1; i < 11; ++i) {
-    if (body.selectPlace[i - 1]) {
-      names = body.selectPlace[i - 1];
+    if (body.nnn[i - 1]) {
+      names = body.nnn[i - 1];
       console.log(names);
       var command = 'update test set place' + i + '=' + "'" + names + "'" + ' order by id DESC LIMIT 1';
       console.log(command);
@@ -133,16 +96,24 @@ app.post('/place', function(req, res) {
 });
 
 // history.html - history 페이지
-app.set('view engine','ejs');
+app.get('/history', function(req, res){
+    fs.readFile('history.ejs', function(error, data){
+        if(error){
+            console.log(error);
+        }else{
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        }
+    });
+});
 
-app.get('/history', function(req, res) {
-  var sql = 'SELECT * FROM test';
-  connection.query(sql,function(err,rows,fields){
-    if (err) {
-      console.log(err);
-    }else{
-      console.log(rows);
-      res.render('history.ejs',{rows: rows});
-    }
-  });
+app.get('/sample', function(req, res){
+    fs.readFile('sample.ejs', function(error, data){
+        if(error){
+            console.log(error);
+        }else{
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        }
+    });
 });
